9a3fabe70cdb35aa95450c454bf7ae21a5354616
kritikafashionpoint <kritikafashionpoint6@gmail.com>
2026-06-10 14:01:09 +0530

    new

--- src/app/categories/[slug]/page.js
+++ src/app/categories/[slug]/page.js
@@ -1,15 +1,68 @@
-import React from 'react'
-import ProductDetailClient from './ProductDetailClient'
+// app/product/[slug]/page.jsx
 
-export const metadata = {
-    title: 'Product Detail',
-    description: 'new'
+import ProductDetailClient from "./ProductDetailClient";
+
+export async function generateMetadata({ params }) {
+
+    const { slug } = await params;
+
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE}/admin/product/slug/${slug}`,
+        {
+            cache: "no-store",
+        }
+    );
+
+    const data = await response.json();
+
+    const product = data?.data;
+
+    return {
+        title:
+            product?.p_meta_title ||
+            product?.p_title ||
+            "Product Detail",
+
+        description:
+            product?.p_meta_description ||
+            product?.p_short_description ||
+            "",
+
+        openGraph: {
+            title:
+                product?.p_meta_title ||
+                product?.p_title,
+
+            description:
+                product?.p_meta_description ||
+                product?.p_short_description,
+
+            images: [
+                product?.index_image,
+            ],
+        },
+
+        twitter: {
+            card: "summary_large_image",
+            title:
+                product?.p_meta_title ||
+                product?.p_title,
+
+            description:
+                product?.p_meta_description ||
+                product?.p_short_description,
+
+            images: [
+                product?.index_image,
+            ],
+        },
+    };
 }
 
-export default function Page() {
+
+export default function Page({ params }) {
+
     return (
-        <div>
-            <ProductDetailClient />
-        </div>
-    )
+        <ProductDetailClient />
+    );
 }
