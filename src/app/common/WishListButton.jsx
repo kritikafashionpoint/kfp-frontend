48c6b0dea9278d03ddf0de7bc6878673a9d500ee
kritikafashionpoint <kritikafashionpoint6@gmail.com>
2026-06-15 12:02:28 +0530

    new

--- src/app/common/WishListButton.jsx
+++ src/app/common/WishListButton.jsx
@@ -8,15 +8,17 @@ import { fetchCartData } from '../redux/thunks/cartThunk'
 import { fetchwishlistData } from '../redux/thunks/wishListThunk'
 
 export default function WishListButton({ ProductBySlug }) {
+    console.log(ProductBySlug)
 
     const token = useSelector((store) => store.user.token)
 
     const wishlist = useSelector(
         (store) => store.wishlist.wishlistData
     );
+    console.log('wishlist',wishlist)
 
     const wishlistCheck = wishlist?.some(
-        (item) => item.product_id === ProductBySlug.id
+        (item) => item.id === ProductBySlug.id
     );
 
     const dispatch = useDispatch()
